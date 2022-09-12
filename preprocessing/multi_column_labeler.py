1b9447211c9cc18185209cbefa0f3a55e33b0feb
Tomasz Mikolajczyk <t.mikolajczyk@gmail.com>
2022-09-12 19:42:36 +0200

    preprocessing

---
+++ preprocessing/multi_column_labeler.py
@@ -0,0 +1,32 @@
+from sklearn.preprocessing import LabelEncoder
+
+
+# source: https://stackoverflow.com/a/58239419/5341370
+class MultiColumnLabelEncoder:
+
+    def __init__(self, columns=None):
+        self.columns = columns  # array of column names to encode
+
+    def fit(self, X, y=None):
+        self.encoders = {}
+        columns = X.columns if self.columns is None else self.columns
+        for col in columns:
+            self.encoders[col] = LabelEncoder().fit(X[col])
+        return self
+
+    def transform(self, X):
+        output = X.copy()
+        columns = X.columns if self.columns is None else self.columns
+        for col in columns:
+            output[col] = self.encoders[col].transform(X[col])
+        return output
+
+    def fit_transform(self, X, y=None):
+        return self.fit(X, y).transform(X)
+
+    def inverse_transform(self, X):
+        output = X.copy()
+        columns = X.columns if self.columns is None else self.columns
+        for col in columns:
+            output[col] = self.encoders[col].inverse_transform(X[col])
+        return output
